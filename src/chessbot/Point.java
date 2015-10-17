8cf8b824e28b4ca8d942c310744eb695848c78bb
Stiven Deleur <deleurapps@gmail.com>
2015-10-17 15:36:26 -0700

    recommit

---
+++ src/chessbot/Point.java
@@ -0,0 +1,22 @@
+package chessbot;
+
+public class Point {
+	int x;
+	int y;
+	public Point(int x, int y){
+		this.x = x;
+		this.y = y;
+	}
+	
+	void setXY(int x, int y){
+		this.x = x;
+		this.y = y;
+	}
+	
+	@Override
+	public String toString() {
+		String[] letters = {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
+		return letters[x] + "" + (y+1);
+	}
+	
+}
