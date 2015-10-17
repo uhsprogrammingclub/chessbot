059b406548bf470f69f50c69af8f47c8867fbf82
Stiven Deleur <deleurapps@gmail.com>
2015-10-17 15:52:37 -0700

    added comments

--- src/chessbot/Point.java
+++ src/chessbot/Point.java
@@ -9,12 +9,14 @@ public class Point {
 	}
 	
 	void setXY(int x, int y){
+		// change point coordinates
 		this.x = x;
 		this.y = y;
 	}
 	
 	@Override
 	public String toString() {
+		//Convert to readable position
 		String[] letters = {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
 		return letters[x] + "" + (y+1);
 	}
